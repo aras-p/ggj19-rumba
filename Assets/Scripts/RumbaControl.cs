ea53a79651c43ba126f4076c080db37ce5b6c704
Aras Pranckevicius <aras@unity3d.com>
2019-01-26 12:50:08 +0200

    Direction choice rotates the roomba a bit

--- Assets/Scripts/RumbaControl.cs
+++ Assets/Scripts/RumbaControl.cs
@@ -5,6 +5,7 @@ using Random = UnityEngine.Random;
 public class RumbaControl : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
+    public float rotateSpeed = 10.0f;
     public Color arrowInactiveColor = new Color(1,1,1,0.25f);
     public Color arrowActiveColor = new Color(1, 0.5f, 0.5f, 1.0f);
     Transform tr;
@@ -24,27 +25,27 @@ public class RumbaControl : MonoBehaviour
     
     void Update()
     {
+    	//var dt = Time.deltaTime;
         var fwd = tr.up * moveSpeed;
         rb.velocity = fwd;
 
         var hor = Input.GetAxis("Horizontal");
         if (hor < 0)
-            directionChoice = -1;
-        else if (hor > 0)
             directionChoice = 1;
+        else if (hor > 0)
+            directionChoice = -1;
         if (Math.Abs(directionChoice) < 0.05f)
             directionChoice = 0;
+
+        rb.angularVelocity = directionChoice * rotateSpeed;
+
         UpdateArrowColors();
     }
 
     void OnCollisionStay2D(Collision2D c)
     {
         var dir = tr.localEulerAngles.z;
-        var newDir = dir + Random.Range(-5,5) - directionChoice * 90;
-        //if (directionChoice < 0)
-        //    newDir = dir + 120;
-        //if (directionChoice > 0)
-        //    newDir = dir - 120;
+        var newDir = dir + Random.Range(-5,5) + directionChoice * 90;
         rb.MoveRotation(newDir);
         directionChoice *= 0.5f;
         UpdateArrowColors();
@@ -52,7 +53,7 @@ public class RumbaControl : MonoBehaviour
 
     void UpdateArrowColors()
     {
-        arrowLeft.color = directionChoice < 0 ? arrowActiveColor : arrowInactiveColor;
-        arrowRight.color = directionChoice > 0 ? arrowActiveColor : arrowInactiveColor;
+        arrowLeft.color = directionChoice > 0 ? arrowActiveColor : arrowInactiveColor;
+        arrowRight.color = directionChoice < 0 ? arrowActiveColor : arrowInactiveColor;
     }
 }
