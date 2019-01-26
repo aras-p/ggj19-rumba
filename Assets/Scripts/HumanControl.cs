46edc6645bdda24c6e1fc47fbfc7e869dd437c2c
Aras Pranckevicius <aras@unity3d.com>
2019-01-26 18:19:00 +0200

    Basic human

---
+++ Assets/Scripts/HumanControl.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class HumanControl : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float intervalMin = 0.3f;
+    public float intervalMax = 2.0f;
+    Transform tr;
+    Rigidbody2D rb;
+    SpriteSwapper swapper;
+    float timeToDecision;
+
+    enum State
+    {
+        Moving,
+        Idle,
+        Count
+    }
+
+    State state = State.Idle;
+
+    void Start()
+    {
+        swapper = GetComponentInChildren<SpriteSwapper>();
+        tr = transform;
+        rb = GetComponent<Rigidbody2D>();
+        NextDecision();
+    }
+    
+    void Update()
+    {
+        var dt = Time.deltaTime;
+        switch (state)
+        {
+        case State.Idle:
+            rb.velocity = Vector2.zero;
+            swapper.enabled = false;
+            break;
+        case State.Moving:
+            var fwd = tr.up * moveSpeed;
+            rb.velocity = fwd;
+            swapper.enabled = true;
+            break;
+        }
+
+        timeToDecision -= dt;
+        if (timeToDecision <= 0)
+            NextDecision();
+    }
+
+    void OnCollisionStay2D(Collision2D c)
+    {
+        NextDecision();
+    }
+
+    void NextDecision()
+    {
+        timeToDecision = Random.Range(intervalMin, intervalMax);
+        var dir = tr.localEulerAngles.z;
+        var newDir = dir + Random.Range(-30.0f, 30.0f);
+        rb.MoveRotation(newDir);
+        state = (State) Random.Range(0, (int)State.Count);
+    }
+}
